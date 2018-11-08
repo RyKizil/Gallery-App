02b53a50a392db372047ad253427fa9854478b3c
RyKizil <eray_kizilmese@hotmail.com>
2018-11-08 08:16:23 +0100

    Add project files.

---
+++ Gallery_App/Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gallery_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gallery_App.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new GalleryIndexModel()
+            {
+
+            };
+            return View(model);
+        }
+    }
+}
---
+++ SimpleImageGallery.Data/SimpleImageGalleryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SimpleImageGallery.Data.Models;
+
+namespace SimpleImageGallery.Data
+{
+    public class SimpleImageGalleryDbContext : DbContext
+    {
+        public SimpleImageGalleryDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<GalleryImage> GalleryImages { get; set; }
+        public DbSet<ImageTag> Imagetags { get; set; }
+    }
+}
